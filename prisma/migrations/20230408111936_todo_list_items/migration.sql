a43cc410913f2f06ffb01b9ea4d5abe405d196fc
Marian Lippold <marian.lippold@gmx.de>
2023-05-24 20:01:17 +0200

    cleaned some stuff

--- prisma/migrations/20230408111936_todo_list_items/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "TodoItem" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "item" TEXT NOT NULL,
-    "date" TEXT NOT NULL,
-    "due" TEXT NOT NULL
-);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,6 @@ export default function Home() {
       <p>
         Go to my business Card if you want to get in touch with me! <Link href='BusinessCard/businessCard'>Profile</Link>
       </p>
-      <p>Go to the ToDo List i created: <Link href="TodoList/todolist">Todo-List</Link></p>
       <p>Here are some CSS-Animations i created, recreated and modified <Link href="CssAnimation/cssanimation">Animations</Link></p>
       <p> REACT-HOOK-FORM <Link href="Showcase/react_hook_form">HERE</Link></p>
 {/* 
